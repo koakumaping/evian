f84ef6c30d268280467c1e19083b37367909f781
平奕峰 <koakumaping@163.com>
2020-09-15 16:13:39 +0800

    Delete filter.ts

--- src/filter.ts
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import moment from 'moment'
-
-Vue.filter('formateDate', function(payload: string): string {
-  if (!payload) return '--'
-  return moment(payload).format('YYYY-MM-DD HH:mm:ss')
-})
